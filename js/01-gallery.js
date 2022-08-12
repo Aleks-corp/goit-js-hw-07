8d6ed2755e061fe854e020cfabf658072d0c5de7
Aleks Haran <nporock@gmail.com>
2022-08-13 02:15:54 +0300

    add-second-variant

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 function galleryContainerMarkupMaker(gallery) {
-  const galleryContainer = gallery
+  return gallery
     .map(
       ({ preview, original, description }) =>
         `<div class="gallery__item">
@@ -17,7 +17,6 @@ function galleryContainerMarkupMaker(gallery) {
   </div>`
     )
     .join('');
-  return galleryContainer;
 }
 
 function onGalleryElClick(evt) {
@@ -29,23 +28,15 @@ function onGalleryElClick(evt) {
 }
 
 function openFullImgInModal(sourceImg) {
-  const instance = basicLightbox.create(
-    `
-    <img src="${sourceImg}" width="800" height="600">
-`,
-    {
-      onShow: (instance) => {
-        document.addEventListener('keydown', onEscapeClose);
-        function onEscapeClose(evt) {
-          if (evt.code === 'Escape') {
-            instance.close();
-            document.removeEventListener('keydown', onEscapeClose);
-          }
-        }
-      },
-    }
-  );
+  instance.element().querySelector('img').src = sourceImg;
   instance.show();
+}
+
+function onEscapeClose(evt) {
+  if (evt.code === 'Escape') {
+    instance.close();
+    document.removeEventListener('keydown', onEscapeClose);
+  }
 }
 
 const galleryEl = document.querySelector('.gallery');
@@ -55,4 +46,74 @@ galleryEl.insertAdjacentHTML(
   galleryContainerMarkupMaker(galleryItems)
 );
 
+const instance = basicLightbox.create(
+  `<img src="#" width="800" height="600">`,
+  {
+    onShow: () => {
+      document.addEventListener('keydown', onEscapeClose);
+    },
+  }
+);
+
 galleryEl.addEventListener('click', onGalleryElClick);
+
+//------Variant 02(old)-----------------------------------------------
+
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+
+// function galleryContainerMarkupMaker(gallery) {
+//   const galleryContainer = gallery
+//     .map(
+//       ({ preview, original, description }) =>
+//         `<div class="gallery__item">
+//     <a class="gallery__link" href="${original}">
+//       <img
+//         class="gallery__image"
+//         src="${preview}"
+//         data-source="${original}"
+//         alt="${description}"
+//       />
+//     </a>
+//   </div>`
+//     )
+//     .join('');
+//   return galleryContainer;
+// }
+
+// function onGalleryElClick(evt) {
+//   evt.preventDefault();
+//   if (!evt.target.classList.contains('gallery__image')) {
+//     return;
+//   }
+//   openFullImgInModal(evt.target.dataset.source);
+// }
+
+// function openFullImgInModal(sourceImg) {
+//   const instance = basicLightbox.create(
+//     `
+//     <img src="${sourceImg}" width="800" height="600">
+// `,
+//     {
+//       onShow: (instance) => {
+//         document.addEventListener('keydown', onEscapeClose);
+//         function onEscapeClose(evt) {
+//           if (evt.code === 'Escape') {
+//             instance.close();
+//             document.removeEventListener('keydown', onEscapeClose);
+//           }
+//         }
+//       },
+//     }
+//   );
+//   instance.show();
+// }
+
+// const galleryEl = document.querySelector('.gallery');
+
+// galleryEl.insertAdjacentHTML(
+//   'beforeend',
+//   galleryContainerMarkupMaker(galleryItems)
+// );
+
+// galleryEl.addEventListener('click', onGalleryElClick);
