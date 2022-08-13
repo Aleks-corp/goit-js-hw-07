04c59c627a7eb11dcc93af0bc3b9fffc3b442c88
Aleks Haran <nporock@gmail.com>
2022-08-13 19:44:30 +0300

    add-simpleLightbox-gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+function galleryContainerMarkupMaker(gallery) {
+  return gallery
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+</a>`
+    )
+    .join('');
+}
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.insertAdjacentHTML(
+  'beforeend',
+  galleryContainerMarkupMaker(galleryItems)
+);
+const lightbox = new SimpleLightbox('.gallery a', {
+  scrollZoom: false,
+  captionDelay: 250,
+  overlayOpacity: 0.75,
+});
